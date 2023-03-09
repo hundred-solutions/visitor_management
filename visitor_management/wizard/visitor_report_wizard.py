a0c9b9771440ce1c854c0d7cc20ae912367c25db
arjun-baidya <arjun.chandra.baidya@gmail.com>
2023-03-09 19:28:59 +0600

    report data problem solve

--- visitor_management/wizard/visitor_report_wizard.py
+++ visitor_management/wizard/visitor_report_wizard.py
@@ -10,9 +10,17 @@ class VisitorReport(models.TransientModel):
     def visitor_report(self):
         check_in_data = self.env['visit.data'].search([('check_in_date', '>=', self.date_from),
                                                        ('check_in_date', '<=', self.date_to)])
+        print("check", check_in_data)
+        name_list = []
+        refrence_employee = []
         for data in check_in_data:
-            datas = {
-                'name': data.v_name.v_name
-            }
-            print("datas", datas)
-        return self.env.ref('visitor_management.visitor_report_action').report_action(self, data=datas)
+            name_list.append(data.v_name.v_name)
+            refrence_employee.append(data.employee_id.name)
+
+        data = {
+            'name': name_list,
+            'refrence_emp': refrence_employee
+        }
+
+        print("datas", data)
+        return self.env.ref('visitor_management.visitor_report_action').report_action(self, data=data)
